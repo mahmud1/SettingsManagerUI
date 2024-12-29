caa33588474c8add9b094ef278c6b7b9eb71846a
mahmud1 <mahmud1@users.noreply.github.com>
2024-12-29 21:54:24 +0100

    initial version

---
+++ setting_manager_ui/setting_ui.py
@@ -0,0 +1,300 @@
+import sys
+
+try:
+    # pyside6
+    from PySide6.QtWidgets import (
+        QApplication,
+        QDialog,
+        QVBoxLayout,
+        QTableWidget,
+        QTableWidgetItem,
+        QTabWidget,
+    )
+    from PySide6.QtCore import Qt, Signal
+    from PySide6.QtGui import QColor, QBrush
+except ImportError:
+    # QGIS
+    from qgis.PyQt.QtWidgets import (
+        QApplication,
+        QDialog,
+        QVBoxLayout,
+        QTableWidget,
+        QTableWidgetItem,
+        QTabWidget,
+    )
+    from qgis.PyQt.QtCore import Qt, pyqtSignal as Signal
+    from qgis.PyQt.QtGui import QColor, QBrush
+
+from json_settings import JsonSettings
+from src.object_with_checkbox import *
+
+
+class SettingsTabWidget(QTableWidget):
+    """
+    A table widget for displaying and editing settings parameters.
+
+    :param section_name: The name of the settings section.
+    :type section_name: str
+    :param params_dict: A dictionary of parameters and their properties.
+    :type params_dict: dict
+    :param parent: The parent widget.
+    :type parent: QWidget, optional
+    """
+
+    def __init__(self, section_name, params_dict, parent=None):
+        super().__init__(parent)
+        self.section_name = section_name
+        self.params_dict = params_dict
+
+        self.param_types = {}
+        self.param_types_defaults = {}
+
+        self.setColumnCount(3)
+        self.setHorizontalHeaderLabels(["Parameter", "Value", "Default"])
+
+        self.verticalHeader().setVisible(False)
+
+        self.loadData()
+
+    def loadData(self):
+        """ Loads the parameters into the table widget. """
+
+        # TODO: implement a way to hide/show certain parameters
+        param_dict_visible = self.params_dict
+
+        self.setRowCount(len(param_dict_visible))
+
+        for row_idx, (param_name, info) in enumerate(param_dict_visible.items()):
+            param_type = info.get("type", "string")
+            param_value = info.get("value", "")
+            param_default = info.get("default", "")
+            auto_flag = info.get("auto", False)
+            options = info.get("options", None)
+
+            # add auto checkbox if auto flag is present
+            add_checkbox = "auto" in info
+
+            # store type and default in a dictionary for later use
+            self.param_types_defaults[param_name] = (param_type, param_default, auto_flag)
+
+            # Column 0: Parameter name (read-only)
+            param_item = QTableWidgetItem(param_name)
+            param_item.setFlags(param_item.flags() & ~Qt.ItemIsEditable)
+            self.setItem(row_idx, 0, param_item)
+            param_item.setBackground(QBrush(QColor(240, 240, 240)))  # Light gray background
+
+            # Column 1: Value (editable)
+            if param_type == "color":
+                wobject = ColorPickerWithCheckbox(param_value, auto_flag, add_checkbox)
+            elif param_type == "bool":
+                wobject = QCheckBox()
+                wobject.setChecked(param_value)
+            elif param_type == "float":
+                wobject = DoubleSpinBoxWithCheckbox(param_value, auto_flag, add_checkbox)
+                wobject.setValue(param_value)
+
+            elif param_type == "int":
+                wobject = SpinBoxWithCheckbox(param_value, auto_flag, add_checkbox)
+                wobject.setValue(param_value)
+
+            elif param_type == "string":
+                wobject = LineEditWithCheckbox(param_value, auto_flag, add_checkbox)
+                wobject.setValue(param_value)
+
+            elif param_type == "dropdown":
+                wobject = ComboBoxWithCheckbox(param_value, auto_flag, add_checkbox)
+                wobject.addItems(options)
+                wobject.setCurrentText(param_value)
+            else:
+                QMessageBox.warning(self, "Unknown parameter type", f"Unknown parameter type: {param_type}")
+                raise ValueError(f"Unknown parameter type: {param_type}")
+
+            self.setCellWidget(row_idx, 1, wobject)
+
+            # Column 2: Default value (read-only)
+            default_item = QTableWidgetItem(str(param_default))
+            default_item.setFlags(default_item.flags() & ~Qt.ItemIsEditable)
+            self.setItem(row_idx, 2, default_item)
+            default_item.setBackground(QBrush(QColor(250, 255, 250)))
+
+
+class SettingsTableDialog(QDialog):
+    """
+    A dialog for displaying and editing settings in a tabbed interface.
+
+    :param json_file: The path to the JSON file containing the settings.
+    :type json_file: str
+    :param block_key: The key for a specific block of settings, defaults to None.
+    :type block_key: str, optional
+    :param parent: The parent widget.
+    :type parent: QWidget, optional
+    """
+
+    # signal emitted when the apply button is clicked
+    applyClicked = Signal()
+
+    def __init__(self, json_file, block_key=None, parent=None):
+        super().__init__(parent)
+        self.json_file = json_file
+        self.block_key = block_key
+        self.settings_dict = {}
+
+        self.settings = JsonSettings(self.json_file)
+        self.setWindowTitle("Settings")
+
+        main_layout = QVBoxLayout(self)
+        self.tab_widget = QTabWidget()
+        main_layout.addWidget(self.tab_widget)
+        button_layout = QHBoxLayout()
+        main_layout.addLayout(button_layout)
+
+        # OK button
+        self.save_button = QPushButton("Ok")
+        self.save_button.clicked.connect(self.onOkClicked)
+        button_layout.addWidget(self.save_button)
+
+        # Apply button
+        self.apply_button = QPushButton("Apply")
+        self.apply_button.clicked.connect(self.onApplyClicked)
+        button_layout.addWidget(self.apply_button)
+
+        # Reset button
+        self.reset_button = QPushButton("Reset")
+        self.reset_button.clicked.connect(lambda: self.resetToDefault(reset_all=False))
+        button_layout.addWidget(self.reset_button)
+
+        # Reset All button
+        self.reset_all_button = QPushButton("Reset All")
+        self.reset_all_button.clicked.connect(lambda: self.resetToDefault(reset_all=True))
+        button_layout.addWidget(self.reset_all_button)
+
+        # Cancel button
+        self.close_button = QPushButton("Cancel")
+        self.close_button.clicked.connect(self.reject)
+        button_layout.addWidget(self.close_button)
+
+        self.loadData()
+
+    def loadData(self):
+        """ Reads the entire JSON file and extracts only the block we care about. """
+        settings_block = self.settings.load(self.block_key)
+
+        for section_name, params_dict in settings_block.items():
+            table = SettingsTabWidget(section_name, params_dict, parent=self.tab_widget)
+            self.tab_widget.addTab(table, section_name)
+
+    def collectData(self):
+        """
+        Collects data from all tabs and updates the JSON block.
+
+        :return: The updated settings block.
+        :rtype: dict
+        """
+        # read JSON
+        settings_block = self.settings.load(self.block_key)
+
+        # collect data from each tab
+        try:
+            for i in range(self.tab_widget.count()):
+                table_widget = self.tab_widget.widget(i)  # a SettingsTabWidget
+                section_name = table_widget.section_name
+
+                # Update the specific part of the JSON file
+                if section_name in settings_block:
+                    for row_index in range(table_widget.rowCount()):
+                        param_name_item = table_widget.item(row_index, 0)
+                        if param_name_item:
+                            param_name = param_name_item.text()
+                            if param_name in settings_block[section_name]:
+                                widget = table_widget.cellWidget(row_index, 1)
+                                if widget:
+                                    if isinstance(widget, QCheckBox):
+                                        settings_block[section_name][param_name]['value'] = widget.isChecked()
+                                    else:
+                                        settings_block[section_name][param_name]['value'] = widget.getValue()
+
+                                    if 'auto' in settings_block[section_name][param_name]:
+                                        settings_block[section_name][param_name]['auto'] = widget.isAuto()
+
+        except ValueError as e:
+            QMessageBox.warning(self, "Invalid Input", str(e))
+            return
+
+        return settings_block
+
+    def resetToDefault(self, reset_all=False):
+        """
+        Resets the settings to their default values.
+
+        :param reset_all: Whether to reset all settings or only the selected ones.
+        :type reset_all: bool
+        """
+
+        if reset_all:
+            for i in range(self.tab_widget.count()):
+                table_widget = self.tab_widget.widget(i)  # Get each SettingsTabWidget
+                self.resetTableWidgetToDefault(table_widget)
+        else:
+            table_widget = self.tab_widget.currentWidget()  # Get the current SettingsTabWidget
+            selected_ranges = table_widget.selectedRanges()  # Get selected ranges from the table widget
+            for selected_range in selected_ranges:
+                for row in range(selected_range.topRow(), selected_range.bottomRow() + 1):
+                    self.resetRowToDefault(table_widget, row)
+
+    def resetTableWidgetToDefault(self, table_widget):
+        """
+        Resets all rows in the given table widget to their default values.
+
+        :param table_widget: The table widget to reset.
+        :type table_widget: SettingsTabWidget
+        """
+        for row in range(table_widget.rowCount()):
+            self.resetRowToDefault(table_widget, row)
+
+    def resetRowToDefault(self, table_widget, row):
+        """
+        Resets a specific row in the given table widget to its default value.
+
+        :param table_widget: The table widget containing the row.
+        :type table_widget: SettingsTabWidget
+        :param row: The row index to reset.
+        :type row: int
+        """
+        param_name_item = table_widget.item(row, 0)
+        if param_name_item:
+            param_name = param_name_item.text()
+            if param_name in table_widget.param_types_defaults:
+                param_type, param_default, auto_flag = table_widget.param_types_defaults[param_name]
+                widget = table_widget.cellWidget(row, 1)
+                if widget:
+                    if isinstance(widget, QCheckBox):
+                        widget.setChecked(param_default)
+                    else:
+                        if widget.checkbox:
+                            widget.checkbox.setChecked(not auto_flag)
+                        else:
+                            widget.setValue(param_default)
+
+    def onOkClicked(self):
+        """ Handles the OK button click event. """
+        self.saveData()
+        self.accept()
+
+    def onApplyClicked(self):
+        """ Handles the Apply button click event. """
+        self.saveData()
+        self.applyClicked.emit()
+
+    def saveData(self):
+        """ Saves the collected data to the JSON file. """
+        settings_block = self.collectData()
+        self.settings.save(self.block_key, settings_block)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    JSON_FILE = "../tests/test.json"
+    dialog = SettingsTableDialog(JSON_FILE, "test setting")
+    dialog.exec()
+    sys.exit(app.exec())
+
---
+++ setting_manager_ui/src/object_with_checkbox.py
@@ -0,0 +1,359 @@
+try:
+    from PySide6.QtWidgets import (
+        QWidget,
+        QPushButton,
+        QHBoxLayout,
+        QMessageBox,
+        QColorDialog,
+        QCheckBox,
+        QSpinBox,
+        QDoubleSpinBox,
+        QSizePolicy,
+        QLineEdit,
+        QComboBox
+    )
+except ImportError:
+    from qgis.PyQt.QtWidgets import (
+        QWidget,
+        QPushButton,
+        QHBoxLayout,
+        QMessageBox,
+        QColorDialog,
+        QCheckBox,
+        QSpinBox,
+        QDoubleSpinBox,
+        QSizePolicy,
+        QLineEdit,
+        QComboBox
+    )
+
+
+class ColorPicker(QWidget):
+    """
+    A widget for selecting colors.
+
+    :param initial_color: The initial color to be set.
+    :type initial_color: str
+    :param parent: The parent widget.
+    :type parent: QWidget, optional
+    """
+    def __init__(self, initial_color, parent=None):
+        super().__init__(parent)
+        self.color = initial_color
+        self.layout = QHBoxLayout(self)
+        self.button = QPushButton()
+        self.button.setStyleSheet(f"background-color: {self.color}")
+        self.button.clicked.connect(self.openColorDialog)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+    def openColorDialog(self):
+        """ Opens a color dialog to select a new color. """
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.color = color.name()
+            self.button.setStyleSheet(f"background-color: {self.color}")
+
+    def getColor(self):
+        """
+        Gets the current color.
+
+        :return: The current color.
+        :rtype: str
+        """
+        return self.color
+
+    def setColor(self, color):
+        """
+        Sets a new color.
+
+        :param color: The new color to be set.
+        :type color: str
+        """
+        self.color = color
+        self.button.setStyleSheet(f"background-color: {self.color}")
+
+
+class ObjectWithCheckbox(QWidget):
+    """
+    A base widget that includes a main widget and an optional checkbox.
+
+    :param value: The initial value of the main widget.
+    :type value: any
+    :param flag: A flag indicating the initial state of the main widget.
+    :type flag: bool
+    :param checkbox: Whether to include a checkbox.
+    :type checkbox: bool, optional
+    :param parent: The parent widget.
+    :type parent: QWidget, optional
+    """
+
+    def __init__(self, value, flag, checkbox=False, parent=None):
+        super().__init__(parent)
+        self.value = value
+        self.layout = QHBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)  # Set layout margins to zero
+        self.wobject = None
+        self.addObject(flag)
+        if checkbox:
+            self.checkbox = QCheckBox()
+            self.checkbox.setChecked(not flag)
+            self.checkbox.stateChanged.connect(self.toggleObject)
+            self.layout.addWidget(self.checkbox)
+            self.layout.addWidget(self.checkbox)
+            self.checkbox.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
+        else:
+            self.checkbox = None
+
+        self.layout.addWidget(self.wobject)
+        self.setLayout(self.layout)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)  # Adjust size policy
+        self.wobject.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+    def addObject(self, flag):
+        """ Adds the main widget. This method should be overridden by subclasses. """
+        pass
+
+    def toggleObject(self, state):
+        """
+        Toggles the enabled state of the main widget.
+
+        :param state: The state of the checkbox.
+        :type state: int
+        """
+
+        self.wobject.setEnabled(state)
+
+    def isAuto(self):
+        """
+        Checks if the checkbox is in the auto state.
+
+        :return: True if the checkbox is in the auto state, False otherwise.
+        :rtype: bool
+        """
+
+        if self.checkbox:
+            return not self.checkbox.isChecked()
+        else:
+            return False
+
+
+class LineEditWithCheckbox(ObjectWithCheckbox):
+    """
+    A widget that includes a QLineEdit and an optional checkbox.
+
+    :param value: The initial text of the QLineEdit.
+    :type value: str
+    :param flag: A flag indicating the initial state of the QLineEdit.
+    :type flag: bool
+    :param checkbox: Whether to include a checkbox.
+    :type checkbox: bool, optional
+    :param parent: The parent widget.
+    :type parent: QWidget, optional
+    """
+    def __init__(self, value, flag, checkbox=False, parent=None):
+        super().__init__(value, flag, checkbox, parent)
+
+    def addObject(self, flag):
+        """ Adds a QLineEdit as the main widget. """
+        self.wobject = QLineEdit()
+        self.wobject.setText(self.value)
+        self.wobject.setEnabled(not flag)
+
+    def setValue(self, value):
+        """
+        Sets the text of the QLineEdit.
+
+        :param value: The new text to be set.
+        :type value: str
+        """
+        self.wobject.setText(value)
+
+    def getValue(self):
+        """
+        Gets the text of the QLineEdit.
+
+        :return: The current text.
+        :rtype: str
+        """
+        return self.wobject.text()
+
+
+class SpinBoxWithCheckbox(ObjectWithCheckbox):
+    """
+    A widget that includes a QSpinBox and an optional checkbox.
+
+    :param value: The initial value of the QSpinBox.
+    :type value: int
+    :param flag: A flag indicating the initial state of the QSpinBox.
+    :type flag: bool
+    :param checkbox: Whether to include a checkbox.
+    :type checkbox: bool, optional
+    :param parent: The parent widget.
+    :type parent: QWidget, optional
+    """
+    def __init__(self, value, flag, checkbox=False, parent=None):
+        super().__init__(value, flag, checkbox, parent)
+
+    def addObject(self, flag):
+        """ Adds a QSpinBox as the main widget. """
+        self.wobject = QSpinBox()
+        self.wobject.setEnabled(not flag)
+
+    def setValue(self, value):
+        """
+        Sets the value of the QSpinBox.
+
+        :param value: The new value to be set.
+        :type value: int
+        """
+        self.wobject.setValue(value)
+
+    def getValue(self):
+        """
+        Gets the value of the QSpinBox.
+
+        :return: The current value.
+        :rtype: int
+        """
+        return self.wobject.value()
+
+
+class DoubleSpinBoxWithCheckbox(ObjectWithCheckbox):
+    """
+    A widget that includes a QDoubleSpinBox and an optional checkbox.
+
+    :param value: The initial value of the QDoubleSpinBox.
+    :type value: float
+    :param flag: A flag indicating the initial state of the QDoubleSpinBox.
+    :type flag: bool
+    :param checkbox: Whether to include a checkbox.
+    :type checkbox: bool, optional
+    :param parent: The parent widget.
+    :type parent: QWidget, optional
+    """
+    def __init__(self, value, flag, checkbox=False, parent=None):
+        super().__init__(value, flag, checkbox, parent)
+
+    def addObject(self, flag):
+        """ Adds a QDoubleSpinBox as the main widget. """
+        self.wobject = QDoubleSpinBox()
+        self.wobject.setEnabled(not flag)
+
+    def setValue(self, value):
+        """
+        Sets the value of the QDoubleSpinBox.
+
+        :param value: The new value to be set.
+        :type value: float
+        """
+        self.wobject.setValue(value)
+
+    def getValue(self):
+        """
+        Gets the value of the QDoubleSpinBox.
+
+        :return: The current value.
+        :rtype: float
+        """
+        return self.wobject.value()
+
+
+class ComboBoxWithCheckbox(ObjectWithCheckbox):
+    """
+    A widget that includes a QComboBox and an optional checkbox.
+
+    :param value: The initial value of the QComboBox.
+    :type value: str
+    :param flag: A flag indicating the initial state of the QComboBox.
+    :type flag: bool
+    :param checkbox: Whether to include a checkbox.
+    :type checkbox: bool, optional
+    :param parent: The parent widget.
+    :type parent: QWidget, optional
+    """
+    def __init__(self, value, flag, checkbox=False, parent=None):
+        super().__init__(value, flag, checkbox, parent)
+
+    def addItems(self, items):
+        """
+        Adds items to the QComboBox.
+
+        :param items: The items to be added.
+        :type items: list of str
+        """
+        self.wobject.addItems(items)
+
+    def setCurrentText(self, text):
+        """
+        Sets the current text of the QComboBox.
+
+        :param text: The text to be set.
+        :type text: str
+        """
+        self.wobject.setCurrentText(text)
+
+    def addObject(self, flag):
+        """ Adds a QComboBox as the main widget. """
+        self.wobject = QComboBox()
+        self.wobject.setEnabled(not flag)
+
+    def setValue(self, value):
+        """
+        Sets the current text of the QComboBox.
+
+        :param value: The text to be set.
+        :type value: str
+        """
+        self.wobject.setCurrentText(value)
+
+    def getValue(self):
+        """
+        Gets the current text of the QComboBox.
+
+        :return: The current text.
+        :rtype: str
+        """
+        return self.wobject.currentText()
+
+
+class ColorPickerWithCheckbox(ObjectWithCheckbox):
+    """
+    A widget that includes a ColorPicker and an optional checkbox.
+
+    :param value: The initial color of the ColorPicker.
+    :type value: str
+    :param flag: A flag indicating the initial state of the ColorPicker.
+    :type flag: bool
+    :param checkbox: Whether to include a checkbox.
+    :type checkbox: bool, optional
+    :param parent: The parent widget.
+    :type parent: QWidget, optional
+    """
+    def __init__(self, value, flag, checkbox=False, parent=None):
+        super().__init__(value, flag, checkbox, parent)
+
+    def addObject(self, flag):
+        """ Adds a ColorPicker as the main widget. """
+        self.wobject = ColorPicker(self.value)
+        self.wobject.setEnabled(not flag)
+
+    def setValue(self, value):
+        """
+        Sets the color of the ColorPicker.
+
+        :param value: The new color to be set.
+        :type value: str
+        """
+        self.wobject.setColor(value)
+
+    def getValue(self):
+        """
+        Gets the color of the ColorPicker.
+
+        :return: The current color.
+        :rtype: str
+        """
+        return self.wobject.getColor()
+
+
